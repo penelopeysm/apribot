017c4eb00de284d3c55f1d618af616a7c3093c33
Penelope Yong <penelopeysm@gmail.com>
2023-05-13 16:35:29 +0100

    Some refactoring to make app cleaner

--- app/Main.hs
+++ app/Main.hs
@@ -23,31 +23,54 @@ import qualified Text.Blaze.Html5.Attributes as A
 import Text.Printf (printf)
 import qualified Web.Scotty as Scotty
 
+data Config = Config
+  { dbFileName :: String,
+    watchedSubreddit :: Text,
+    notifyOnPostId :: ID Post,
+    port :: Int,
+    keywords :: [Text],
+    userAgent :: Text
+  }
+
+-- | App configuration.
+config :: Config
+config =
+  Config
+    { dbFileName = "/data/posts.db",
+      watchedSubreddit = "pokemontrades",
+      notifyOnPostId = PostID "13fzjhf",
+      port = 8080,
+      keywords = ["apri", "dream", "beast", "safari", "sport", "fast", "friend", "heavy", "level", "love", "lure", "moon"],
+      userAgent = "github:penelopeysm/apribot by /u/is_a_togekiss"
+    }
+
 getEnvAsText :: Text -> IO Text
 getEnvAsText = fmap T.pack . getEnv . T.unpack
 
 atomically :: MVar () -> IO () -> IO ()
 atomically lock action = withMVar lock $ const action
 
--- | Very basic check on whether a string is found in either the
--- post title or the post body.
+-- | Checks whether a string is found in either the post title or the post body.
 hasWord :: Post -> Text -> Bool
 hasWord post term =
   term `T.isInfixOf` T.toCaseFold (postTitle post)
     || term `T.isInfixOf` T.toCaseFold (postBody post)
 
--- | Keywords triggering post reporting
-keywords :: [Text]
-keywords = ["apri", "dream", "beast", "safari", "sport", "fast", "friend", "heavy", "level", "love", "lure", "moon"]
+-- | TODO: Replace with deep learning
+isHit :: Post -> Bool
+isHit post = any (hasWord post) (keywords config)
 
+-- | In principle, more characters should be escaped. However, the only texts
+-- we're escaping (fornow) are Reddit usernames, and I think the underscore is
+-- the only character that is both allowed in a username and has a special
+-- meaning in Markdown.
 markdownEscape :: Text -> Text
 markdownEscape = T.replace "_" "\\_"
 
--- | Process newly seen posts
+-- | Process newly seen posts.
 process :: MVar () -> Post -> RedditT (MVar ())
 process lock post = do
-  let target = PostID "13fzjhf"
-  let postBody =
+  let commentText =
         printf
           "**Title:** [%s](%s)\n\n**Poster:** [\\/u\\/%s](https://reddit.com/user/%s)\n\n**Tag:** %s\n\n**Posted at:** %s"
           (postTitle post)
@@ -56,21 +79,25 @@ process lock post = do
           (postAuthor post)
           (fromMaybe "None" (postFlairText post))
           (show $ postCreatedTime post)
-  if any (hasWord post) keywords
+  if isHit post
     then do
-      addNewComment target (T.pack postBody)
-      liftIO $ addToDb post True
-      liftIO $ atomically lock $ T.putStrLn $ "Notifying about post " <> unPostID (postId post) <> "\n" <> postTitle post <> "\n" <> postUrl post <> "\n"
+      addNewComment (notifyOnPostId config) (T.pack commentText)
+      liftIO $ do
+        addToDb post True
+        atomically lock $
+          printf "Hit: %s\n%s\n%s\n" (unPostID (postId post)) (postTitle post) (postUrl post)
     else do
-      liftIO $ atomically lock $ T.putStrLn $ "Found non-matching post\n" <> postTitle post <> "\n" <> postUrl post <> "\n"
-      liftIO $ addToDb post False
+      liftIO $ do
+        addToDb post False
+        atomically lock $
+          printf "Non-hit: %s\n%s\n%s\n" (unPostID (postId post)) (postTitle post) (postUrl post)
   pure lock
 
 -- | Add a post to the SQLite database. The Bool parameter indicates whether it
 -- was a hit or not.
 addToDb :: Post -> Bool -> IO ()
-addToDb post isHit = do
-  sql <- getDataFileName "/data/test.db" >>= open
+addToDb post hit = do
+  sql <- getDataFileName (dbFileName config) >>= open
   execute_ sql "CREATE TABLE IF NOT EXISTS posts (id TEXT PRIMARY KEY, url TEXT, title TEXT, submitter TEXT, isHit INTEGER, time TEXT, flair TEXT)"
   executeNamed
     sql
@@ -79,7 +106,7 @@ addToDb post isHit = do
       ":url" := postUrl post,
       ":title" := postTitle post,
       ":submitter" := postAuthor post,
-      ":isHit" := (if isHit then 1 else 0 :: Integer),
+      ":isHit" := (if hit then 1 else 0 :: Integer),
       ":time" := formatTime defaultTimeLocale "%F %T" (postCreatedTime post),
       ":flair" := fromMaybe "" (postFlairText post)
     ]
@@ -88,7 +115,7 @@ addToDb post isHit = do
 -- | Generate HTML from the SQLite database
 makeHtml :: IO Html
 makeHtml = do
-  sql <- getDataFileName "/data/test.db" >>= open
+  sql <- getDataFileName (dbFileName config) >>= open
   hits <- query_ sql "SELECT id, url, title, submitter, time, flair FROM posts WHERE isHit = 1 ORDER BY time DESC LIMIT 50" :: IO [(Text, Text, Text, Text, Text, Text)]
   nonhits <- query_ sql "SELECT id, url, title, submitter, time, flair FROM posts WHERE isHit = 0 ORDER BY time DESC LIMIT 50" :: IO [(Text, Text, Text, Text, Text, Text)]
   -- Total number of rows
@@ -96,7 +123,6 @@ makeHtml = do
   -- Total number of hits
   m <- fromOnly . Prelude.head <$> (query_ sql "SELECT COUNT(*) FROM posts WHERE isHit = 1;" :: IO [Only Int])
   close sql
-
   let mkTableRow :: (Text, Text, Text, Text, Text, Text) -> Html
       mkTableRow (pid, purl, ptitle, psubmitter, ptime, pflair) =
         H.tr $
@@ -124,7 +150,8 @@ makeHtml = do
           H.a ! A.href "https://reddit.com/r/pokemontrades" $ "/r/pokemontrades"
           " subreddit and identifies potential Aprimon-related threads. "
           "It does so by scanning for certain keywords in either the post title or body. "
-          "This is admittedly not very sophisticated, and leads to quite a few false positives; in the long term, it would be nice to have a better algorithm for this."
+          "This is admittedly not very sophisticated, and leads to quite a few false positives. "
+          "In the long term, it would be nice to have a better algorithm for this."
         H.p $ do
           toHtml (printf "So far, ApriBot has seen a total of %d posts, " n :: String)
           toHtml (printf "of which %d were hits (%.2f%%)." m (100 * fromIntegral m / fromIntegral n :: Double) :: String)
@@ -154,8 +181,8 @@ makeHtml = do
 web :: MVar () -> IO ()
 web lock = do
   css <- getDataFileName "static/styles.css"
-  atomically lock $ T.putStrLn "Launching web server on port 8080..."
-  Scotty.scotty 8080 $ do
+  atomically lock $ printf "Launching web server on port %d...\n" (port config)
+  Scotty.scotty (port config) $ do
     Scotty.get "/" $ do
       liftIO makeHtml >>= Scotty.html . renderHtml
     Scotty.get "/static/styles.css" $ do
@@ -170,12 +197,11 @@ bot lock = do
   credsPassword <- getEnvAsText "REDDIT_PASSWORD"
   credsClientId <- getEnvAsText "REDDIT_ID"
   credsClientSecret <- getEnvAsText "REDDIT_SECRET"
-  let userAgent = "github:penelopeysm/apribot by /u/is_a_togekiss"
   let creds = Credentials {..}
-  env <- authenticate creds userAgent
+  env <- authenticate creds (userAgent config)
   let protected = do
         catch
-          (runRedditT' env $ postStream defaultStreamSettings process lock "pokemontrades")
+          (runRedditT' env $ postStream defaultStreamSettings process lock (watchedSubreddit config))
           ( \(e :: SomeException) -> do
               atomically lock $ T.hPutStrLn stderr ("Exception: " <> T.pack (show e))
               threadDelay 5000000
@@ -183,6 +209,7 @@ bot lock = do
           )
   protected
 
+-- | Entry point
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
