78f2522580754b67312a3e38e5d0ad82af52ef75
Penelope Yong <penelopeysm@gmail.com>
2023-05-14 02:28:41 +0100

    Update database schema

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@ module Main where
 import Control.Concurrent (MVar, forkIO, newMVar, threadDelay, withMVar)
 import Control.Exception (SomeException, catch)
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad (forM_)
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -98,30 +99,33 @@ process lock post = do
 addToDb :: Post -> Bool -> IO ()
 addToDb post hit = do
   sql <- getDataFileName (dbFileName config) >>= open
-  execute_ sql "CREATE TABLE IF NOT EXISTS posts (id TEXT PRIMARY KEY, url TEXT, title TEXT, submitter TEXT, isHit INTEGER, time TEXT, flair TEXT)"
   executeNamed
     sql
-    "INSERT INTO posts (id, url, title, submitter, isHit, time, flair) VALUES (:id, :url, :title, :submitter, :isHit, :time, :flair)"
+    "INSERT INTO posts (id, url, title, body, submitter, hit, time, flair, agree, disagree) \
+    \ VALUES (:id, :url, :title, :body, :submitter, :hit, :time, :flair, '', '')"
     [ ":id" := unPostID (postId post),
       ":url" := postUrl post,
       ":title" := postTitle post,
+      ":body" := postBody post,
       ":submitter" := postAuthor post,
-      ":isHit" := (if hit then 1 else 0 :: Integer),
+      ":hit" := (if hit then 1 else 0 :: Integer),
       ":time" := formatTime defaultTimeLocale "%F %T" (postCreatedTime post),
       ":flair" := fromMaybe "" (postFlairText post)
     ]
+  n <- fromOnly . Prelude.head <$> (query_ sql "SELECT COUNT(*) FROM posts;" :: IO [Only Int])
   close sql
+  printf "Added post to database (%d total posts)\n" n
 
 -- | Generate HTML from the SQLite database
 makeHtml :: IO Html
 makeHtml = do
   sql <- getDataFileName (dbFileName config) >>= open
-  hits <- query_ sql "SELECT id, url, title, submitter, time, flair FROM posts WHERE isHit = 1 ORDER BY time DESC LIMIT 50" :: IO [(Text, Text, Text, Text, Text, Text)]
-  nonhits <- query_ sql "SELECT id, url, title, submitter, time, flair FROM posts WHERE isHit = 0 ORDER BY time DESC LIMIT 50" :: IO [(Text, Text, Text, Text, Text, Text)]
+  hits <- query_ sql "SELECT id, url, title, submitter, time, flair FROM posts WHERE hit = 1 ORDER BY time DESC LIMIT 50" :: IO [(Text, Text, Text, Text, Text, Text)]
+  nonhits <- query_ sql "SELECT id, url, title, submitter, time, flair FROM posts WHERE hit = 0 ORDER BY time DESC LIMIT 50" :: IO [(Text, Text, Text, Text, Text, Text)]
   -- Total number of rows
-  n <- fromOnly . Prelude.head <$> (query_ sql "SELECT COUNT(*) FROM posts;" :: IO [Only Int])
+  n <- fromOnly . Prelude.head <$> (query_ sql "SELECT COUNT(*) FROM posts WHERE hit = 1 or hit = 0;" :: IO [Only Int])
   -- Total number of hits
-  m <- fromOnly . Prelude.head <$> (query_ sql "SELECT COUNT(*) FROM posts WHERE isHit = 1;" :: IO [Only Int])
+  m <- fromOnly . Prelude.head <$> (query_ sql "SELECT COUNT(*) FROM posts WHERE hit = 1;" :: IO [Only Int])
   close sql
   let mkTableRow :: (Text, Text, Text, Text, Text, Text) -> Html
       mkTableRow (pid, purl, ptitle, psubmitter, ptime, pflair) =
@@ -148,20 +152,21 @@ makeHtml = do
         H.p $ do
           "ApriBot monitors new posts on the "
           H.a ! A.href "https://reddit.com/r/pokemontrades" $ "/r/pokemontrades"
-          " subreddit and identifies potential Aprimon-related threads. "
-          "It does so by scanning for certain keywords in either the post title or body. "
-          "This is admittedly not very sophisticated, and leads to quite a few false positives. "
-          "In the long term, it would be nice to have a better algorithm for this."
+          " subreddit and identifies potential Aprimon-related threads."
+          " It does so by scanning for certain keywords in either the post title or body."
+          " This is admittedly not very sophisticated, and leads to quite a few false positives."
+          " In the long term, it would be nice to have a better algorithm for this."
         H.p $ do
-          toHtml (printf "So far, ApriBot has seen a total of %d posts, " n :: String)
-          toHtml (printf "of which %d were hits (%.2f%%)." m (100 * fromIntegral m / fromIntegral n :: Double) :: String)
-          "This page shows you the most recent 50 hits and non-hits, ordered by most recent first."
+          toHtml (printf "So far, ApriBot has processed a total of %d posts," n :: String)
+          toHtml (printf " of which %d were hits (%.2f%%)." m (100 * fromIntegral m / fromIntegral n :: Double) :: String)
+          " This page shows you the most recent 50 hits and non-hits, ordered by most recent first."
         H.p $ do
-          "If you have any questions about ApriBot, feel free to get in touch with "
-          H.a ! A.href "https://reddit.com/u/is_a_togekiss" $ "/u/is_a_togekiss"
-          "; she doesn’t have DMs enabled, so just comment on any of her posts."
+          "If you have any questions about ApriBot, feel free to get in touch with"
+          H.a ! A.href " https://reddit.com/u/is_a_togekiss" $ "/u/is_a_togekiss"
+          "; she doesn’t have DMs enabled, so just comment on any of her trade posts."
         H.p $ do
-          "ApriBot is implemented with Haskell and SQLite. If you’re interested, its source code is on "
+          "ApriBot is implemented with Haskell and SQLite."
+          " If you’re interested, its source code is on "
           H.a ! A.href "https://github.com/penelopeysm/apribot" $ "GitHub"
           "."
       H.h2 "Hits"
@@ -209,10 +214,72 @@ bot lock = do
           )
   protected
 
+-- | Set up the database from scratch. Do not use this unless you want to start
+-- all over again...!
+--
+-- Database schema:
+--    - id         : Post ID.
+--    - url        : Post URL.
+--    - title      : Post title.
+--    - body       : Post body.
+--    - submitter  : Post submitter.
+--    - hit        : 1 or 0, indicates whether the bot reported it or not.
+--                   If null, the bot didn't process it.
+--    - time       : Post creation time in UTC.
+--    - flair      : Post flair text. Empty if unflaired.
+--    - truth      : 1 or 0, indicates whether the post was really of interest.
+--                   This has to be manually determined by human volunteers (I
+--                   think). If null, the truth has not been determined.
+--    - agree      : Comma-separated list of people who think the post is
+--                   interesting.
+--    - disagree   : Comma-separated list of people who think the post is
+--                   NOT interesting.
+populateDB :: IO ()
+populateDB = do
+  sql <- getDataFileName (dbFileName config) >>= open
+  execute_ sql "DROP TABLE posts"
+  execute_ sql "CREATE TABLE IF NOT EXISTS posts\
+                \ (id TEXT NOT NULL PRIMARY KEY,\
+                \ url TEXT NOT NULL,\
+                \ title TEXT NOT NULL,\
+                \ body TEXT NOT NULL, \
+                \ submitter TEXT NOT NULL,\
+                \ time TEXT NOT NULL,\
+                \ flair TEXT NOT NULL, \
+                \ hit INTEGER,\
+                \ truth INTEGER, \
+                \ agree TEXT NOT NULL, \
+                \ disagree TEXT NOT NULL);"
+  credsUsername <- getEnvAsText "REDDIT_USERNAME"
+  credsPassword <- getEnvAsText "REDDIT_PASSWORD"
+  credsClientId <- getEnvAsText "REDDIT_ID"
+  credsClientSecret <- getEnvAsText "REDDIT_SECRET"
+  let creds = Credentials {..}
+  env <- authenticate creds (userAgent config)
+
+  runRedditT' env $ do
+    last1000Posts <- subredditPosts 1000 "pokemontrades" New
+    forM_ last1000Posts $ \post -> liftIO $ do
+      executeNamed
+        sql
+        "INSERT INTO posts (id, url, title, body, submitter, time, flair, agree, disagree) \
+        \ VALUES (:id, :url, :title, :body, :submitter, :time, :flair, '', '')"
+        [ ":id" := unPostID (postId post),
+          ":url" := postUrl post,
+          ":title" := postTitle post,
+          ":body" := postBody post,
+          ":submitter" := postAuthor post,
+          ":time" := formatTime defaultTimeLocale "%F %T" (postCreatedTime post),
+          ":flair" := fromMaybe "" (postFlairText post)
+        ]
+
+  close sql
+
 -- | Entry point
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
+  -- Run bot and web server concurrently
   lock <- newMVar ()
   _ <- forkIO $ web lock
   bot lock
